0fedb04f0c004e87de0d3ae629f22b8b0d9b29da
Jacky Chen <jackychen19860504@gmail.com>
2017-11-05 01:48:02 -0500

    Add files via upload

---
+++ calculatingCircleArea.java
@@ -0,0 +1,10 @@
+
+public class calculatingCircleArea {
+	
+	public static void main(String[] arge){
+		int radius = 3;
+		System.out.println(Math.PI);
+		double area = radius * radius * Math.PI;
+		System.out.println("The area id: "+area);
+	}
+}
---
+++ rectangleAreaFunction.java
@@ -0,0 +1,11 @@
+
+public class rectangleAreaFunction {
+	
+	public static void main(String[] args){
+		System.out.print("The rectangle area is : "+area(3,5));	
+	}
+	
+	public static float area(int length, int width){
+		return length * width;
+	}
+}
